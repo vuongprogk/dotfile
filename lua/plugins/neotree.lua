947e719a39b00786205576bfdb2c1a5120655793
vuongprogk <vuong753333@gmail.com>
2024-02-12 23:21:10 +0700

    optimize file and config

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -3,6 +3,6 @@ return {
   branch = "v3.x",
   dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim" },
   config = function()
-    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal current toggle<CR>', {silent = true})
+    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal current<CR>', {silent = true})
   end
 }
