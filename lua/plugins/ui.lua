4cdf6ccaf9a7a4e56c964ac8b7adeedca9c10977
vuongprogk <vuong753333@gmail.com>
2024-02-13 12:23:28 +0700

    add noice and optimize file

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,78 @@
+return {
+  --bufferline
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        mode = "tabs",
+        show_close_icon = false,
+      },
+    },
+  },
+  -- statusline
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      options = {
+        -- globalstatus = false,
+        theme = "horizon",
+      },
+    },
+  },
+
+  --setup nui
+  {
+
+    "MunifTanjim/nui.nvim",
+    opts = nil,
+  },
+  --setup noice
+  {
+    "folke/noice.nvim",
+    opts = {
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+      routes = {
+        {
+          filter = {
+            event = "msg_show",
+            any = {
+              { find = "%d+L, %d+B" },
+              { find = "; after #%d+" },
+              { find = "; before #%d+" },
+            },
+          },
+          view = "mini",
+        },
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        inc_rename = true,
+      },
+    },
+  },
+  -- setup notify
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 3000,
+      max_height = function()
+        return math.floor(vim.o.lines * 0.75)
+      end,
+      max_width = function()
+        return math.floor(vim.o.columns * 0.75)
+      end,
+      on_open = function(win)
+        vim.api.nvim_win_set_config(win, { zindex = 100 })
+      end,
+      fps = 60,
+    },
+  },
+}
--- lua/vimsetup.lua
+++ lua/vimsetup.lua
@@ -1,31 +1,29 @@
 -- Set up for neovide
 if vim.g.neovide then
-	vim.opt.linespace = 0
-	vim.g.neovide_scale_factor = 1.0
-	local alpha = function()
-		return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
-	end
-	vim.g.neovide_transparency = 0.9
-	vim.g.transparency = 0.9
-	vim.g.neovide_background_color = "#0f1117" .. alpha()
-	vim.g.neovide_scroll_animation_far_lines = 3
-	vim.g.neovide_hide_mouse_when_typing = true
-	vim.g.neovide_refresh_rate_idle = 5
-	vim.g.neovide_refresh_rate = 60
+  vim.opt.linespace = 0
+  vim.g.neovide_scale_factor = 1.0
+  local alpha = function()
+    return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
+  end
+  vim.g.neovide_transparency = 0.9
+  vim.g.transparency = 0.9
+  vim.g.neovide_background_color = "#0f1117" .. alpha()
+  vim.g.neovide_scroll_animation_far_lines = 3
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_refresh_rate_idle = 5
+  vim.g.neovide_refresh_rate = 60
 end
-vim.o.guifont = "JetBrains Mono:h13"
+vim.opt.guifont = "JetBrains Mono:h13"
 vim.opt.termguicolors = true
-vim.cmd("set scrolloff=15")
-vim.cmd("set number")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set autoindent")
-vim.cmd("set expandtab")
-vim.cmd("set clipboard+=unnamedplus")
-vim.cmd("set nocompatible")
+vim.opt.scrolloff = 15
+vim.opt.number = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.autoindent = true
+vim.opt.expandtab = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.compatible = false
 vim.g.mapleader = " "
--- set up for coc nvim
---vim.g.coc_global_extensions = {'coc-css', 'coc-clangd', 'coc-pyright','coc-sumneko-lua','coc-java','coc-rust-analyzer'}
 vim.keymap.set("n", "<C-l>", ":tabnext<CR>", { silent = true })
 vim.keymap.set("n", "<C-h>", ":tabprevious<CR>", { silent = true })
