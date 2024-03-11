5316f2e560b3822a697edb28d1ad1ab982640674
vuongprogk <vuong753333@gmail.com>
2024-03-11 15:23:24 +0700

    Configure update for Java development

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,155 +1,151 @@
 return {
-  -- setup formatting
-  {
-    "nvimtools/none-ls.nvim",
-    config = function()
-      local null_ls = require("null-ls")
-      null_ls.setup({
-        sources = {
-          null_ls.builtins.formatting.stylua,
-          null_ls.builtins.diagnostics.pylint.with({
-            diagnostics_postprocess = function(diagnostic)
-              diagnostic.code = diagnostic.message_id
-            end,
-          }),
-          null_ls.builtins.formatting.isort,
-          null_ls.builtins.formatting.black,
-          null_ls.builtins.formatting.prettier,
-          null_ls.builtins.formatting.clang_format,
-          null_ls.builtins.diagnostics.cpplint,
-        },
-      })
-      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { silent = true })
-    end,
-  },
-  -- setup nvim cmp for completion
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "neovim/nvim-lspconfig",
-      "williamboman/mason-lspconfig.nvim",
-      "hrsh7th/cmp-nvim-lsp",
-      "williamboman/nvim-lsp-installer",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
-      "windwp/nvim-autopairs",
-    },
-    config = function()
-      -- server lsp config and ensure installed in mason
-      local servers = {
-        "clangd",
-        "tsserver",
-        "lua_ls",
-        "pyright",
-        "jdtls",
-      }
+	-- setup formatting
+	{
+		"nvimtools/none-ls.nvim",
+		config = function()
+			local null_ls = require("null-ls")
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.diagnostics.pylint.with({
+						diagnostics_postprocess = function(diagnostic)
+							diagnostic.code = diagnostic.message_id
+						end,
+					}),
+					null_ls.builtins.formatting.isort,
+					null_ls.builtins.formatting.black,
+					null_ls.builtins.formatting.prettier,
+					null_ls.builtins.formatting.clang_format,
+					null_ls.builtins.diagnostics.cpplint,
+				},
+			})
+			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { silent = true })
+		end,
+	},
+	-- setup nvim cmp for completion
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"neovim/nvim-lspconfig",
+			"williamboman/mason-lspconfig.nvim",
+			"hrsh7th/cmp-nvim-lsp",
+			"williamboman/nvim-lsp-installer",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+			"windwp/nvim-autopairs",
+		},
+		config = function()
+			-- server lsp config and ensure installed in mason
+			local servers = {
+				"clangd",
+				"tsserver",
+				"lua_ls",
+				"pyright",
+			}
 
-      local cmp = require("cmp")
-      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			local cmp = require("cmp")
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 
-      -- setup for autopairs
-      require("nvim-autopairs").setup({})
-      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-      -- call mason setup
-      require("mason").setup()
-      -- call mason config
-      require("mason-lspconfig").setup({
-        ensure_installed = servers,
-        automatic_installation = true,
-      })
-      -- call nvim-lsp-installer for automatic installation lsp server
-      require("nvim-lsp-installer").setup({
-        automatic_installation = true,
-        ui = {
-          icons = {
-            server_installed = "✓",
-            server_pending = "➜",
-            server_uninstalled = "✗",
-          },
-        },
-      })
+			-- setup for autopairs
+			require("nvim-autopairs").setup({})
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+			-- call mason setup
+			require("mason").setup()
+			-- call mason config
+			require("mason-lspconfig").setup({
+				ensure_installed = servers,
+				automatic_installation = true,
+			})
+			-- call nvim-lsp-installer for automatic installation lsp server
+			require("nvim-lsp-installer").setup({
+				automatic_installation = true,
+				ui = {
+					icons = {
+						server_installed = "✓",
+						server_pending = "➜",
+						server_uninstalled = "✗",
+					},
+				},
+			})
 
-      --cmp setup
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({
-            select = true,
-          }),
-        }),
-        sources = cmp.config.sources({
-          {
-            name = "nvim_lsp",
-          },
-          {
-            name = "luasnip",
-          },
-          {
-            name = "buffer",
-          },
-          {
-            name = "path",
-          },
-        }),
-      })
+			--cmp setup
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({
+						select = true,
+					}),
+				}),
+				sources = cmp.config.sources({
+					{
+						name = "nvim_lsp",
+					},
+					{
+						name = "luasnip",
+					},
+					{
+						name = "buffer",
+					},
+					{
+						name = "path",
+					},
+				}),
+			})
 
-      -- set keymap for completion
-      vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
-      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-      vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-      vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
-      vim.api.nvim_create_autocmd("LspAttach", {
-        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-        callback = function(ev)
-          -- Enable completion triggered by <c-x><c-o>
-          vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+			-- set keymap for completion
+			vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+			vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+			vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+				callback = function(ev)
+					-- Enable completion triggered by <c-x><c-o>
+					vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-          -- Buffer local mappings.
-          -- See `:help vim.lsp.*` for documentation on any of the below functions
-          local opts = { buffer = ev.buf }
-          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-          vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-          vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
-          vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
-          vim.keymap.set("n", "<leader>wl", function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-          end, opts)
-          vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
-          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-          vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
-          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-          vim.keymap.set("n", "<leader>f", function()
-            vim.lsp.buf.format({ async = true })
-          end, opts)
-        end,
-      })
+					-- Buffer local mappings.
+					-- See `:help vim.lsp.*` for documentation on any of the below functions
+					local opts = { buffer = ev.buf }
+					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+					vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+					vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+					vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+					vim.keymap.set("n", "<leader>wl", function()
+						print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+					end, opts)
+					vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+					vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+				end,
+			})
 
-      -- setup cmp lsp using capabilities
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
-      for _, lsp in ipairs(servers) do
-        lspconfig[lsp].setup({
-          --on_attach = my_custom_on_attach,
-          capabilities = capabilities,
-        })
-      end
-    end,
-  },
+			-- setup cmp lsp using capabilities
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
+			for _, lsp in ipairs(servers) do
+				lspconfig[lsp].setup({
+					--on_attach = my_custom_on_attach,
+					capabilities = capabilities,
+				})
+			end
+		end,
+	},
 }
--- lua/vimsetup.lua
+++ lua/vimsetup.lua
@@ -1,28 +1,28 @@
 -- Set up for neovide
 if vim.g.neovide then
-	vim.opt.linespace = 0
-	vim.g.neovide_scale_factor = 1.0
-	local alpha = function()
-		return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
-	end
-	vim.g.neovide_transparency = 0.9
-	vim.g.transparency = 0.9
-	vim.g.neovide_background_color = "#0f1117" .. alpha()
-	vim.g.neovide_scroll_animation_far_lines = 3
-	vim.g.neovide_hide_mouse_when_typing = true
-	vim.g.neovide_refresh_rate_idle = 5
-	vim.g.neovide_refresh_rate = 60
+  vim.opt.linespace = 0
+  vim.g.neovide_scale_factor = 1.0
+  local alpha = function()
+    return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
+  end
+  vim.g.neovide_transparency = 0.9
+  vim.g.transparency = 0.9
+  vim.g.neovide_background_color = "#0f1117" .. alpha()
+  vim.g.neovide_scroll_animation_far_lines = 3
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_refresh_rate_idle = 5
+  vim.g.neovide_refresh_rate = 60
+  vim.opt.guifont = "JetBrains Mono:h13"
 end
 vim.opt.belloff = "all"
-vim.opt.guifont = "JetBrains Mono:h13"
 vim.opt.termguicolors = true
 vim.opt.scrolloff = 15
 vim.opt.number = true
+vim.opt.expandtab = true
+vim.opt.autoindent = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
-vim.opt.autoindent = true
-vim.opt.expandtab = true
 vim.opt.clipboard = "unnamedplus"
 vim.opt.compatible = false
 vim.keymap.set("n", "<space>", "<NOP>", { silent = true, remap = false })
