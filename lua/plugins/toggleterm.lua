89b83d5dc1aa03e76d0712ebe6563847c2b6f41b
vuongprogk <vuong753333@gmail.com>
2024-04-04 15:33:11 +0700

    change shell to PowerShell

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -14,7 +14,7 @@ return {
 			persist_size = true,
 			direction = "horizontal",
 			close_on_exit = true, -- close the terminal window when the process exits
-			shell = vim.o.shell,
+			shell = "PowerShell",
 			auto_scroll = true, -- automatically scroll to the bottom on terminal output
 			float_opts = {
 				border = "curved",
