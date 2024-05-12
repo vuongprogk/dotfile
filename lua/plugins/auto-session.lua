0643e095da1c0dc45900737d437e8c75f1a5b734
vuongprogk <vuong753333@gmail.com>
2024-05-13 02:40:25 +0700

    update auto session

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -1,18 +1,20 @@
 return {
-  "rmagatti/auto-session",
-  config = function()
-    require("auto-session").setup({
-      auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-      session_lens = {
-        -- If load_on_setup is set to false, one needs to eventually call `require("auto-session").setup_session_lens()` if they want to use session-lens.
-        buftypes_to_ignore = {}, -- list of buffer types what should not be deleted from current session
-        load_on_setup = true,
-        theme_conf = { border = true },
-        previewer = false,
-      },
-      vim.keymap.set("n", "<Leader>ls", require("auto-session.session-lens").search_session, {
-        noremap = true,
-      }),
-    })
-  end,
+	"rmagatti/auto-session",
+	config = function()
+		local auto_session = require("auto-session")
+
+		auto_session.setup({
+			auto_restore_enabled = false,
+			auto_session_suppress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
+		})
+
+		local keymap = vim.keymap
+
+		keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" })
+		keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" })
+		keymap.set("n", "<leader>ws", "<cmd>SessionDelete<CR>", { desc = "Delete session" })
+		keymap.set("n", "<Leader>ls", require("auto-session.session-lens").search_session, {
+			noremap = true,
+		})
+	end,
 }
