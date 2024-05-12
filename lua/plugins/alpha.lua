aeddaf42e95175dbd57a43541fe248dc7563d186
vuongprogk <vuong753333@gmail.com>
2024-05-13 02:44:18 +0700

    update dashboard

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,29 +1,38 @@
 return {
-	"goolord/alpha-nvim",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
+  "goolord/alpha-nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
 
-	config = function()
-		local alpha = require("alpha")
-		local dashboard = require("alpha.themes.dashboard")
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.dashboard")
 
-		dashboard.section.header.val = {
-			[[                        ]],
-			[[                        ]],
-			[[                        ]],
-			[[                        ]],
-			[[ █████╗  ██████╗███████╗]],
-			[[██╔══██╗██╔════╝██╔════╝]],
-			[[███████║██║     █████╗  ]],
-			[[██╔══██║██║     ██╔══╝  ]],
-			[[██║  ██║╚██████╗███████╗]],
-			[[╚═╝  ╚═╝ ╚═════╝╚══════╝]],
-			[[                        ]],
-			[[                        ]],
-			[[                        ]],
-		}
+    dashboard.section.header.val = {
+      [[                        ]],
+      [[                        ]],
+      [[                        ]],
+      [[                        ]],
+      [[ █████╗  ██████╗███████╗]],
+      [[██╔══██╗██╔════╝██╔════╝]],
+      [[███████║██║     █████╗  ]],
+      [[██╔══██║██║     ██╔══╝  ]],
+      [[██║  ██║╚██████╗███████╗]],
+      [[╚═╝  ╚═╝ ╚═════╝╚══════╝]],
+      [[                        ]],
+      [[                        ]],
+      [[                        ]],
+    }
+    dashboard.section.buttons.val = {
+      dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
+      dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+      dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
+      dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
+      dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
+      dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
+    }
+    alpha.setup(dashboard.opts)
 
-		alpha.setup(dashboard.opts)
-	end,
+    vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+  end,
 }
