67e4dddaf1d1068c098e1201872fec2922eac709
vuongprogk <vuong753333@gmail.com>
2024-05-17 10:59:30 +0700

    add more utillities

--- lua/ace/plugins/ui.lua
+++ lua/ace/plugins/ui.lua
@@ -1,95 +1,92 @@
 return {
-  --bufferline
-  {
-    "akinsho/bufferline.nvim",
-    opts = {
-      options = {
-        mode = "tabs",
-        show_close_icon = false,
-      },
-    },
-  },
-  -- statusline
-  {
-    "nvim-lualine/lualine.nvim",
-    opts = {
-      options = {
-        -- globalstatus = false,
-        theme = "horizon",
-      },
-    },
-  },
+	--bufferline
+	{
+		"akinsho/bufferline.nvim",
+		opts = {
+			options = {
+				mode = "tabs",
+				show_close_icon = false,
+			},
+		},
+	},
+	-- statusline
+	{
+		"nvim-lualine/lualine.nvim",
+		opts = {
+			options = {
+				-- globalstatus = false,
+				theme = "horizon",
+			},
+		},
+	},
 
-  --setup nui
-  {
+	--setup nui
+	{
 
-    "MunifTanjim/nui.nvim",
-    opts = nil,
-  },
-  --setup noice
-  {
-    "folke/noice.nvim",
-    opts = {
-      lsp = {
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
-        },
-      },
-      routes = {
-        {
-          filter = {
-            event = "msg_show",
-            any = {
-              { find = "%d+L, %d+B" },
-              { find = "; after #%d+" },
-              { find = "; before #%d+" },
-            },
-          },
-          view = "mini",
-        },
-      },
-      presets = {
-        bottom_search = true,
-        command_palette = true,
-        long_message_to_split = true,
-        inc_rename = true,
-      },
-      views = {
-        mini = {
-          win_options = {
-            winblend = 0,
-          },
-        },
-      },
-    },
-  },
-  -- setup notify
-  {
-    "rcarriga/nvim-notify",
-    keys = {
-      {
-        "<leader>un",
-        function()
-          require("notify").dismiss({ silent = true, pending = true })
-        end,
-        desc = "Dismiss All Notifications",
-      },
-    },
-    opts = {
-      stages = "static",
-      timeout = 3000,
-      fps = 60,
-      max_height = function()
-        return math.floor(vim.o.lines * 0.75)
-      end,
-      max_width = function()
-        return math.floor(vim.o.columns * 0.75)
-      end,
-      on_open = function(win)
-        vim.api.nvim_win_set_config(win, { zindex = 100 })
-      end,
-    },
-  },
+		"MunifTanjim/nui.nvim",
+		opts = nil,
+	},
+	--setup noice
+	{
+		"folke/noice.nvim",
+		opts = {
+			lsp = {
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true,
+				},
+				progress = {
+					enabled = false,
+				},
+				message = {
+					enabled = false,
+				},
+			},
+			routes = {
+				{
+					filter = {
+						event = "msg_show",
+						any = {
+							{ find = "%d+L, %d+B" },
+							{ find = "; after #%d+" },
+							{ find = "; before #%d+" },
+						},
+					},
+					view = "mini",
+				},
+			},
+			presets = {
+				bottom_search = true,
+				command_palette = true,
+				long_message_to_split = true,
+				inc_rename = true,
+			},
+			views = {
+				mini = {
+					win_options = {
+						winblend = 0,
+					},
+				},
+			},
+		},
+	},
+	-- setup notify
+	{
+		"rcarriga/nvim-notify",
+		keys = {
+			{
+				"<leader>un",
+				function()
+					require("notify").dismiss({ silent = true, pending = true })
+				end,
+				desc = "Dismiss All Notifications",
+			},
+		},
+		opts = {
+			stages = "static",
+			timeout = 3000,
+			fps = 60,
+		},
+	},
 }
