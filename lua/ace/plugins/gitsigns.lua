0690dfd3f757eedff292c09bdf60d57bfaf352d6
ace <vuong753333@gmail.com>
2024-06-18 06:07:39 +0700

    add gitsigns to dotfile

---
+++ lua/ace/plugins/gitsigns.lua
@@ -0,0 +1,8 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  config = function()
+    local gitsigns = require("gitsigns")
+    gitsigns.setup()
+    vim.keymap.set("n", "<leader>tb", gitsigns.toggle_current_line_blame)
+  end,
+}
