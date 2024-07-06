4acd756cd91bd2e714f80c6ebb59e6e9711cddc1
vuongprogk <vuong753333@gmail.com>
2024-07-07 05:33:18 +0700

    update format and notify

--- lua/ace/plugins/formatting.lua
+++ lua/ace/plugins/formatting.lua
@@ -1,45 +1,49 @@
 return {
-	"stevearc/conform.nvim",
-	keys = {
-		{
-			"<leader>gf",
-			function()
-				require("conform").format({
-					lsp_fallback = true,
-					async = false,
-					timeout_ms = 1000,
-				})
-			end,
-			{
-				desc = "formatting code",
-				mode = { "n", "v" },
-			},
-		},
-	},
-	opts = {
-		formatters_by_ft = {
-			lua = { "stylua" },
-			python = { "isort", "black" },
-			javascript = { { "prettierd", "prettier" } },
-			java = {
-				"clang_format",
-			},
-			cpp = {
-				"clang_format",
-			},
-			html = { "prettier" },
-			cs = { "csharpier" },
-			css = { "prettier" },
-		},
-	},
-	format_on_save = {
-		timeout_ms = 500,
-		lsp_format = "fallback",
-	},
-	formatters = {
-		csharpier = {
-			command = "dotnet-csharpier",
-			args = { "--write-stdout" },
-		},
-	},
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  keys = {
+    {
+      "<leader>gf",
+      function()
+        require("conform").format({
+          lsp_fallback = true,
+          async = false,
+          timeout_ms = 1000,
+        })
+      end,
+      {
+        desc = "formatting code",
+        mode = { "n", "v" },
+      },
+    },
+  },
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      python = { "isort", "black" },
+      javascript = { { "prettierd", "prettier" } },
+      java = {
+        "clang_format",
+      },
+      cpp = {
+        "clang_format",
+      },
+      html = { "prettier" },
+      cs = { "csharpier" },
+      css = { "prettier" },
+    },
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_format = "prefer",
+    },
+    format_after_save = {
+      lsp_format = "fallback",
+    },
+    formatters = {
+      csharpier = {
+        command = "dotnet-csharpier",
+        args = { "--write-stdout" },
+      },
+    },
+  },
 }
