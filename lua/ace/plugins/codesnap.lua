774d9ea571fc7cb11f3d36f80d41cf86fa73dc01
vuongprogk <vuong753333@gmail.com>
2024-07-12 21:30:13 +0700

    Turn off codesnap on Windows

--- lua/ace/plugins/codesnap.lua
+++ lua/ace/plugins/codesnap.lua
@@ -1,11 +1,17 @@
 return {
-	"mistricky/codesnap.nvim",
-	build = "make",
-	cmd = { "CodeSnapSave", "CodeSnapSaveHighlight" },
-	opts = {
-		save_path = "/mnt/c/Users/vuong/Pictures/Codesnap",
-		has_breadcrumbs = false,
-		watermark = "Powered by ACE",
-		bg_theme = "dusk",
-	},
+  "mistricky/codesnap.nvim",
+  enabled = function()
+    if require("ace.custom.os").getName() == 'Windows' then
+      return false
+    end
+    return true
+  end,
+  build = "make",
+  cmd = { "CodeSnapSave", "CodeSnapSaveHighlight" },
+  opts = {
+    save_path = "/mnt/c/Users/vuong/Pictures/Codesnap",
+    has_breadcrumbs = false,
+    watermark = "Powered by ACE",
+    bg_theme = "dusk",
+  },
 }
