60ddb8856b32bb4d00e05a34ff9fa48405cefc6a
vuongprogk <vuong753333@gmail.com>
2024-07-12 21:29:55 +0700

    Update opacity of FTerm

--- lua/ace/plugins/FTerm.lua
+++ lua/ace/plugins/FTerm.lua
@@ -1,22 +1,23 @@
 return {
-	"numToStr/FTerm.nvim",
-	config = function()
-		local term = require("FTerm")
-		vim.keymap.set({ "n", "i", "t" }, "<C-\\>", term.toggle, { desc = "Toogle FTerm" })
-		term.setup({
-			blend = 30,
-		})
-		local lazygit = term:new({
+  "numToStr/FTerm.nvim",
+  event = "VimEnter",
+  config = function()
+    local term = require("FTerm")
+    vim.keymap.set({ "n", "i", "t" }, "<C-\\>", term.toggle, { desc = "Toogle FTerm" })
+    term.setup({
+      blend = 0,
+    })
+    local lazygit = term:new({
 
-			ft = "fterm_lazygit", -- You can also override the default filetype, if you want
-			cmd = "lazygit",
-			dimensions = {
-				height = 0.9,
-				width = 0.9,
-			},
-		})
-		vim.keymap.set({ "n" }, "<leader>lg", function()
-			lazygit:toggle()
-		end, { desc = "Togle lazygit" })
-	end,
+      ft = "fterm_lazygit", -- You can also override the default filetype, if you want
+      cmd = "lazygit",
+      dimensions = {
+        height = 0.9,
+        width = 0.9,
+      },
+    })
+    vim.keymap.set({ "n" }, "<leader>lg", function()
+      lazygit:toggle()
+    end, { desc = "Togle lazygit" })
+  end,
 }
