86ec33e266396b33ea0c40125cec32eac6412771
vuongprogk <vuong753333@gmail.com>
2024-07-13 14:04:31 +0700

    Disable check luarock

--- lua/ace/lazy.lua
+++ lua/ace/lazy.lua
@@ -1,25 +1,29 @@
 -- Set up neovim using lazy
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
+local plugin_path = {
+	{ import = "ace.plugins" },
+	{ import = "ace.lsp" },
+}
 require("lazy").setup({
-  { import = "ace.plugins" },
-  { import = "ace.lsp" },
-}, {
-  checker = {
-    enable = true,
-    notify = false,
-  },
-  change_detection = {
-    notify = false,
-  },
+	spec = plugin_path,
+	pgk = { enable = false },
+	rocks = { enable = false },
+	checker = {
+		enable = true,
+		notify = false,
+	},
+	change_detection = {
+		notify = false,
+	},
 })
