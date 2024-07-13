0befa42a082765aa18cf564a6533a058a71d250e
vuongprogk <vuong753333@gmail.com>
2024-07-13 17:35:22 +0700

    Add tailwindcss support

---
+++ lua/ace/lsp/tailwind.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		"NvChad/nvim-colorizer.lua",
+		opts = {
+			user_default_options = {
+				tailwind = true,
+			},
+		},
+	},
+	{
+		"roobert/tailwindcss-colorizer-cmp.nvim",
+		opts = {
+			color_square_width = 1,
+		},
+	},
+}
--- lua/ace/plugins/nvim-cmp.lua
+++ lua/ace/plugins/nvim-cmp.lua
@@ -1,124 +1,109 @@
----@diagnostic disable: missing-fields
 return {
-  "hrsh7th/nvim-cmp",
-  -- event = { "InsertEnter", "BufRead" },
-  dependencies = {
-    "hrsh7th/cmp-buffer", -- source for text in buffer
-    "neovim/nvim-lspconfig",
-    "hrsh7th/cmp-path",   -- source for file system paths
-    {
-      "L3MON4D3/LuaSnip",
-      build = function()
-        if require("ace.custom.os").getName() == 'Windows' then
-          vim.notify("Can't install jsregexp on window")
-          return nil
-        end
-        return "make install_jsregexp"
-      end
-    },
-    "saadparwaiz1/cmp_luasnip", -- for autocompletion
-    {
-      "garymjr/nvim-snippets",
-      opts = {
-        friendly_snippets = true,
-      },
-      dependencies = { "rafamadriz/friendly-snippets" },
-    },
-    "onsails/lspkind.nvim", -- vs-code like pictograms
-    {
-      "hrsh7th/cmp-nvim-lua",
-      ft = "lua",
-    },
-    "hrsh7th/cmp-cmdline",
-  },
-  config = function()
-    local lspkind = require("lspkind")
-    require("luasnip.loaders.from_vscode").lazy_load()
-    local cmp = require("cmp")
-    local luasnip = require("luasnip")
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		"hrsh7th/cmp-buffer", -- source for text in buffer
+		"neovim/nvim-lspconfig",
+		"hrsh7th/cmp-path", -- source for file system paths
+		{
+			"L3MON4D3/LuaSnip",
+			build = function()
+				if require("ace.custom.os").getName() == "Windows" then
+					vim.notify("Can't install jsregexp on window")
+					return nil
+				end
+				return "make install_jsregexp"
+			end,
+		},
+		"saadparwaiz1/cmp_luasnip", -- for autocompletion
+		{
+			"garymjr/nvim-snippets",
+			opts = {
+				friendly_snippets = true,
+			},
+			dependencies = { "rafamadriz/friendly-snippets" },
+		},
+		"onsails/lspkind.nvim", -- vs-code like pictograms
+		{
+			"hrsh7th/cmp-nvim-lua",
+			ft = "lua",
+		},
+	},
+	config = function()
+		local lspkind = require("lspkind")
+		require("luasnip.loaders.from_vscode").lazy_load()
+		local cmp = require("cmp")
+		local luasnip = require("luasnip")
 
-    -- old config
-    cmp.setup({
-      mapping = cmp.mapping.preset.insert({
-        ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-        ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-        ["<C-e>"] = cmp.mapping.abort(),        -- close completion window
-        ["<CR>"] = cmp.mapping.confirm({ select = false }),
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      }),
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
-      window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-      },
-      sources = cmp.config.sources({
-        {
-          name = "lazydev",
-          group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-        },
-        {
-          name = "nvim_lua",
-          options = {
-            include_deprecated = true,
-          },
-        },
-        {
-          name = "nvim_lsp",
-        },
-        {
-          name = "luasnip",
-        },
-        {
-          name = "buffer",
-        },
-        {
-          name = "path",
-        },
-      }),
-      formatting = {
-        format = lspkind.cmp_format({
-          mode = "symbol_text",
-          maxwidth = 50,
-          ellipsis_char = "...",
-          menu = {
-            buffer = "[Buffer]",
-            nvim_lsp = "[LSP]",
-            luasnip = "[LuaSnip]",
-            nvim_lua = "[Lua]",
-            latex_symbols = "[Latex]",
-          },
-        }),
-      },
-    })
-    cmp.setup.cmdline({ "/", "?" }, {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = "buffer" },
-      },
-    })
-  end,
+		cmp.setup({
+			mapping = cmp.mapping.preset.insert({
+				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+				["<C-e>"] = cmp.mapping.abort(), -- close completion window
+				["<CR>"] = cmp.mapping.confirm({ select = false }),
+				["<Tab>"] = cmp.mapping(function(fallback) -- super tab
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+			}),
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
+			sources = cmp.config.sources({
+				{
+					name = "lazydev",
+					group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+				},
+				{
+					name = "nvim_lua",
+					options = {
+						include_deprecated = true,
+					},
+				},
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+				{ name = "buffer" },
+				{ name = "path" },
+			}),
+			formatting = {
+				fields = { "kind", "abbr", "menu" },
+				format = lspkind.cmp_format({
+					mode = "symbol_text",
+					maxwidth = 50,
+					ellipsis_char = "...",
+					menu = {
+						buffer = "[Buffer]",
+						nvim_lsp = "[LSP]",
+						luasnip = "[LuaSnip]",
+						nvim_lua = "[Lua]",
+						latex_symbols = "[Latex]",
+					},
+					before = require("tailwindcss-colorizer-cmp").formatter,
+				}),
+			},
+		})
+	end,
 }
