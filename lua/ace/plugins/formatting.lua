f00a562556ec31db8c430b09c4697083a388c0c4
ace <vuong753333@gmail.com>
2024-08-10 00:32:06 +0700

    feat: Update conform config

--- lua/ace/plugins/formatting.lua
+++ lua/ace/plugins/formatting.lua
@@ -28,8 +28,8 @@ return {
 		formatters_by_ft = {
 			lua = { "stylua" },
 			python = { "isort", "black" },
-			javascript = { { "prettierd", "prettier" } },
-			javascriptreact = { { "prettierd", "prettier" } },
+			javascript = { "prettierd", "prettier", stop_after_first = true },
+			javascriptreact = { "prettierd", "prettier", stop_after_first = true },
 
 			java = {
 				"clang_format",
@@ -40,14 +40,14 @@ return {
 			html = { "prettier" },
 			cs = { "csharpier" },
 			css = { "prettier" },
-			cshtml = { "csharpier", "prettier" },
+			cshtml = { "csharpier", "prettier", stop_after_first = true },
 		},
 		-- format_on_save = {
 		-- 	timeout_ms = 500,
 		-- 	lsp_format = "first",
 		-- },
 		format_on_save = {
-			lsp_format = "first",
+			lsp_format = "fallback",
 			timeout_ms = 500,
 		},
 		formatters = {
