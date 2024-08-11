79f90e47f17ba784a47d6b2c9ed31dc19d45aee0
ace <vuong753333@gmail.com>
2024-08-11 16:26:23 +0700

    chore: Telescope config make it faster

--- lua/ace/plugins/telescope.lua
+++ lua/ace/plugins/telescope.lua
@@ -1,26 +1,71 @@
 return {
 	{
 		"nvim-telescope/telescope.nvim",
+		event = "VeryLazy",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
-			"nvim-telescope/telescope-ui-select.nvim",
-			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 		},
 		version = false,
-		config = function()
-			local telescope = require("telescope")
-			local actions = require("telescope.actions")
-			telescope.setup({
-				defaults = {
-					mappings = {
-						i = {
-							["<C-k>"] = actions.move_selection_previous, -- move to prev result
-							["<C-j>"] = actions.move_selection_next, -- move to next result
-						},
+		opts = function(_, opts)
+			opts.defaults = {
+				mappings = {
+					i = {
+						["<C-k>"] = require("telescope.actions").move_selection_previous, -- move to prev result
+						["<C-j>"] = require("telescope.actions").move_selection_next, -- move to next result
 					},
 				},
-				extensions = {
-					["ui-select"] = {
+			}
+			local keymap = vim.keymap
+			vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", {})
+			vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", {})
+			vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>", {})
+			vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", {})
+			keymap.set({ "n", "v" }, "<leader>ft", "<cmd>TodoTelescope<CR>", { desc = "Find todo comment" })
+			keymap.set({ "n", "v" }, "<leader>nh", "<cmd>Telescope notify<CR>", {
+				desc = "Notification history",
+			})
+			keymap.set({ "n", "v" }, "<leader>nt", "<cmd>Telescope noice<CR>", {
+				desc = "Noice history",
+			})
+		end,
+	},
+	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		dependencies = {
+			{
+				"nvim-telescope/telescope.nvim",
+				opts = function(_, opts)
+					if not opts.extensions then
+						opts.extensions = {}
+					end
+					opts.extensions.fzf = {
+						fuzzy = true, -- false will only do exact matching
+						override_generic_sorter = true, -- override the generic sorter
+						override_file_sorter = true, -- override the file sorter
+						case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+						-- the default case_mode is "smart_case"
+					}
+				end,
+			},
+		},
+		event = "VeryLazy",
+		build = "make",
+		init = function()
+			if vim.tbl_get(require("lazy.core.config"), "plugins", "telescope.nvim", "_", "loaded") ~= nil then
+				local ok, err = pcall(require("telescope").load_extension, "fzf")
+				if not ok then
+					vim.notify("Failed to load `telescope-fzf-native.nvim`:\n" .. err, "error", {})
+				end
+			end
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		dependencies = {
+			{
+				"nvim-telescope/telescope.nvim",
+				opts = function(_, opts)
+					opts.extensions["ui-select"] = {
 						theme = "dropdown",
 						results_title = false,
 
@@ -44,34 +89,18 @@ return {
 							results = { "─", "│", "─", "│", "├", "┤", "╯", "╰" },
 							preview = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
 						},
-					},
-					fzf = {
-						fuzzy = true, -- false will only do exact matching
-						override_generic_sorter = true, -- override the generic sorter
-						override_file_sorter = true, -- override the file sorter
-						case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-						-- the default case_mode is "smart_case"
-					},
-				},
-			})
-			telescope.load_extension("fzf")
-			telescope.load_extension("ui-select")
-			telescope.load_extension("notify")
-			telescope.load_extension("noice")
-
-			local keymap = vim.keymap
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-			keymap.set({ "n", "v" }, "<leader>ft", "<cmd>TodoTelescope<CR>", { desc = "Find todo comment" })
-			keymap.set({ "n", "v" }, "<leader>nh", "<cmd>Telescope notify<CR>", {
-				desc = "Notification history",
-			})
-			keymap.set({ "n", "v" }, "<leader>nt", "<cmd>Telescope noice<CR>", {
-				desc = "Noice history",
-			})
+					}
+				end,
+			},
+		},
+		event = "VeryLazy",
+		init = function()
+			if vim.tbl_get(require("lazy.core.config"), "plugins", "telescope.nvim", "_", "loaded") ~= nil then
+				local ok, err = pcall(require("telescope").load_extension, "ui-select")
+				if not ok then
+					vim.notify("Failed to load `telescope-ui-select.nvim`:\n" .. err, "error", {})
+				end
+			end
 		end,
 	},
 }
