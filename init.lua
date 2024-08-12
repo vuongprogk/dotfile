355621d18d9e986570fadfa8a0031d01a202534d
ace <vuong753333@gmail.com>
2024-08-12 19:18:09 +0700

    feat: Adding neovide config and loader

--- init.lua
+++ init.lua
@@ -1,4 +1,11 @@
+if vim.loader then
+	vim.loader.enable()
+end
+vim.g.enabled_neovide = false
+if vim.g.neovide then
+	require("ace.core.neovide")
+	vim.g.enabled_neovide = true
+end
 require("ace.core")
 -- setup lazy
 require("ace.lazy")
-
