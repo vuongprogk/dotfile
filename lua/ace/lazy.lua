1589c406e3d289cd265fbb78861c3c48ed436903
ace <vuong753333@gmail.com>
2024-08-14 14:57:45 +0700

    chore: Enable checker of plugin manager

--- lua/ace/lazy.lua
+++ lua/ace/lazy.lua
@@ -53,10 +53,4 @@ require("lazy").setup({
 			},
 		},
 	},
-	checker = {
-		enabled = false,
-		notify = true, -- get a notification when new updates are found
-		frequency = 3600, -- check for updates every hour
-		check_pinned = false, -- check for pinned packages that can't be updated
-	},
 })
