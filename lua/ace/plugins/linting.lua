3736f6fc1cd3efcae42bfb46406cb3159107b5c9
ace <vuong753333@gmail.com>
2024-08-18 23:52:45 +0700

    chore: Using linting manually

--- lua/ace/plugins/linting.lua
+++ lua/ace/plugins/linting.lua
@@ -20,14 +20,6 @@ return {
 	config = function()
 		local lint = require("lint")
 		-- TODO create auto command ran after Buffer Wrote
-		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-
-		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-			group = lint_augroup,
-			callback = function()
-				lint.try_lint()
-			end,
-		})
 		vim.keymap.set("n", "<leader>l", function()
 			lint.try_lint()
 		end, { desc = "Trigger linting for current file" })
