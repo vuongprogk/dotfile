7f870bacd9c7915c7b6e75b7b3cd7e64f5cc48c1
ace <vuong753333@gmail.com>
2024-08-20 05:45:33 +0700

    feat: add new feature of bufferline

--- lua/ace/plugins/ui.lua
+++ lua/ace/plugins/ui.lua
@@ -6,12 +6,42 @@ return {
 		"akinsho/bufferline.nvim",
 		event = "VeryLazy",
 		dependencies = "nvim-tree/nvim-web-devicons",
+		keys = {
+			{ "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
+			{ "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete Other Buffers" },
+			{ "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
+			{ "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
+			{ "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+			{ "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+		},
 		opts = {
 			options = {
-				mode = "tabs",
-				show_close_icon = false,
+				diagnostics = "nvim_lsp",
+				always_show_bufferline = false,
+				diagnostics_indicator = function(_, _, diag)
+					local icons = {
+						Error = " ",
+						Warn = " ",
+						Hint = " ",
+						Info = " ",
+					}
+					local ret = (diag.error and icons.Error .. diag.error .. " " or "")
+						.. (diag.warning and icons.Warn .. diag.warning or "")
+					return vim.trim(ret)
+				end,
 			},
 		},
+		config = function(_, opts)
+			require("bufferline").setup(opts)
+			-- Fix bufferline when restoring a session
+			vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete" }, {
+				callback = function()
+					vim.schedule(function()
+						pcall(nvim_bufferline)
+					end)
+				end,
+			})
+		end,
 	},
 	-- statusline
 	{
@@ -50,8 +80,9 @@ return {
 						},
 					}
 				end,
+				disabled_filetypes = { statusline = { "alpha" } },
+				extensions = { "nvim-tree" },
 			},
-			extensions = { "nvim-tree" },
 		},
 	},
 	-- setup noice
