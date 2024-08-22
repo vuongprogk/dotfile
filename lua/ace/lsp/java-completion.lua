a107f9fc8ffc6c58d71b79d3c62bff5d25123a80
ace <vuong753333@gmail.com>
2024-08-22 16:24:42 +0700

    feat: Removing maven plugin

--- lua/ace/lsp/java-completion.lua
+++ lua/ace/lsp/java-completion.lua
@@ -54,14 +54,4 @@ return {
 			})
 		end,
 	},
-	{
-		"eatgrass/maven.nvim",
-		cmd = { "Maven", "MavenExec" },
-		dependencies = "nvim-lua/plenary.nvim",
-		config = function()
-			require("maven").setup({
-				executable = "mvnw",
-			})
-		end,
-	},
 }
