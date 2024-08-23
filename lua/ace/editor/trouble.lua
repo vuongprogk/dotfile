f4ac97e793f9300e0183a46b69b464b18c537bbc
ace <vuong753333@gmail.com>
2024-08-23 22:15:54 +0700

    feat: add trouble support

---
+++ lua/ace/editor/trouble.lua
@@ -0,0 +1,54 @@
+return {
+	"folke/trouble.nvim",
+	cmd = { "Trouble" },
+	opts = {
+		auto_close = true, -- auto close when there are no items
+		modes = {
+			lsp = {
+				win = { position = "right" },
+			},
+		},
+	},
+	keys = {
+		{ "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)", remap = true },
+		{
+			"<leader>xX",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+			desc = "Buffer Diagnostics (Trouble)",
+			remap = true,
+		},
+		{ "<leader>cs", "<cmd>Trouble symbols toggle<cr>", desc = "Symbols (Trouble)", remap = true },
+		{ "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)", remap = true },
+		{ "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)", remap = true },
+		{
+			"[q",
+			function()
+				if require("trouble").is_open() then
+					require("trouble").prev({ skip_groups = true, jump = true })
+				else
+					local ok, err = pcall(vim.cmd.cprev)
+					if not ok then
+						vim.notify(err, vim.log.levels.ERROR)
+					end
+				end
+			end,
+			desc = "Previous Trouble/Quickfix Item",
+			remap = true,
+		},
+		{
+			"]q",
+			function()
+				if require("trouble").is_open() then
+					require("trouble").next({ skip_groups = true, jump = true })
+				else
+					local ok, err = pcall(vim.cmd.cnext)
+					if not ok then
+						vim.notify(err, vim.log.levels.ERROR)
+					end
+				end
+			end,
+			desc = "Next Trouble/Quickfix Item",
+			remap = true,
+		},
+	},
+}
