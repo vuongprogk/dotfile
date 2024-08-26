76b3924a942f4d8bf199c5d694ecd5b3f075a584
ace <vuong753333@gmail.com>
2024-08-26 11:40:04 +0700

    refactor: update some stuff

---
+++ lua/ace/plugins/nvim-colorizer.lua
@@ -0,0 +1,12 @@
+return {
+	{
+		"NvChad/nvim-colorizer.lua",
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {
+			user_default_options = {
+				names = false,
+				tailwind = true,
+			},
+		},
+	},
+}
