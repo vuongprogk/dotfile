5bf560bce66b079cea38604c377eb8bc08523d4d
ace <vuong753333@gmail.com>
2024-08-26 11:38:12 +0700

    feat: change terminal to toggleterm

---
+++ lua/ace/utils/terminal.lua
@@ -0,0 +1,17 @@
+return {
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		event = "VeryLazy",
+		opts = {
+			open_mapping = [[<c-\>]],
+			direction = "float",
+			shell = Ace.is_win() and "powershell /nologo" or "bash",
+			float_opts = {
+				border = "curved",
+				winblend = 0,
+				title_pos = "center",
+			},
+		},
+	},
+}
