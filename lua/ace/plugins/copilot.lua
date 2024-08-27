4dca476d3dba3b2b9fb69b322b048101c5896d2d
ace <vuong753333@gmail.com>
2024-08-27 16:34:34 +0700

    refactor: add icon copilot to lualine

--- lua/ace/plugins/copilot.lua
+++ lua/ace/plugins/copilot.lua
@@ -13,16 +13,62 @@ return {
 		},
 	},
 	{
+		"nvim-lualine/lualine.nvim",
+		optional = true,
+		event = "VeryLazy",
+		opts = function(_, opts)
+			local colors = {
+				[""] = Ace.ui.fg("Special"),
+				["Normal"] = Ace.ui.fg("Special"),
+				["Warning"] = Ace.ui.fg("DiagnosticError"),
+				["InProgress"] = Ace.ui.fg("DiagnosticWarn"),
+			}
+			table.insert(opts.sections.lualine_y, 1, {
+				function()
+					local icon = Ace.config.icons.kinds.Copilot
+					local status = require("copilot.api").status.data
+					return icon .. (status.message or "")
+				end,
+				cond = function()
+					if not package.loaded["copilot"] then
+						return
+					end
+					local ok, clients = pcall(Ace.lsp.get_clients, { name = "copilot", bufnr = 0 })
+					if not ok then
+						return false
+					end
+					return ok and #clients > 0
+				end,
+				color = function()
+					if not package.loaded["copilot"] then
+						return
+					end
+					local status = require("copilot.api").status.data
+					return colors[status.status] or colors[""]
+				end,
+			})
+		end,
+	},
+	{
 		"nvim-cmp",
 		dependencies = {
 			{
 				"zbirenbaum/copilot-cmp",
 				dependencies = "copilot.lua",
 				opts = {},
+				config = function(_, opts)
+					local copilot_cmp = require("copilot_cmp")
+					copilot_cmp.setup(opts)
+					-- attach cmp source whenever copilot attaches
+					-- fixes lazy-loading issues with the copilot cmp source
+					Ace.lsp.on_attach(function(client)
+						copilot_cmp._on_insert_enter({})
+					end, "copilot")
+				end,
 			},
 		},
 		opts = function(_, opts)
-			table.insert(opts.sources, 1, {
+			table.insert(opts.sources, {
 				name = "copilot",
 				priority = 750,
 			})
