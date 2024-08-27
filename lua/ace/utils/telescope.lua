ad50b811aec8390df22aa730f9ae08f6dbcb89cb
ace <vuong753333@gmail.com>
2024-08-27 16:34:57 +0700

    refactor: remove fzf integration from telescope

--- lua/ace/utils/telescope.lua
+++ lua/ace/utils/telescope.lua
@@ -4,21 +4,13 @@ return {
 		cmd = "Telescope",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
-			{
-				"nvim-telescope/telescope-fzf-native.nvim",
-				build = "make",
-			},
 		},
 		version = false,
 		keys = {
-			{ "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Telescope find file", remap = true } },
-			{ "<leader>fg", "<cmd>Telescope live_grep<CR>", { desc = "Telescope find word in file", remap = true } },
-			{
-				"<leader>fb",
-				"<cmd>Telescope buffers<CR>",
-				{ desc = "Telescope find buffers is opening", remap = true },
-			},
-			{ "<leader>fh", "<cmd>Telescope help_tag<CR>", { desc = "Telescope find help tag", remap = true } },
+			{ "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Find file", remap = true } },
+			{ "<leader>fg", "<cmd>Telescope live_grep<CR>", { desc = "Find word in file", remap = true } },
+			{ "<leader>fb", "<cmd>Telescope buffers<CR>", { desc = "Find buffers is opening", remap = true } },
+			{ "<leader>fh", "<cmd>Telescope help_tag<CR>", { desc = "Find help tag", remap = true } },
 			{ "<leader>ft", "<cmd>TodoTelescope<CR>", { desc = "Find todo comment", remap = true } },
 			{ "<leader>nh", "<cmd>Telescope notify<CR>", { desc = "Notification history", remap = true } },
 			{ "<leader>nt", "<cmd>Telescope noice<CR>", { desc = "Noice history", remap = true } },
@@ -66,22 +58,7 @@ return {
 						hidden = true,
 					},
 				},
-				extensions = {
-					fzf = {
-						fuzzy = true, -- false will only do exact matching
-						override_generic_sorter = true, -- override the generic sorter
-						override_file_sorter = true, -- override the file sorter
-						case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-						-- the default case_mode is "smart_case"
-					},
-				},
 			}
-		end,
-		config = function()
-			local ok_fzf, err_fzf = pcall(require("telescope").load_extension, "fzf")
-			if not ok_fzf then
-				vim.notify("Failed to load `telescope-fzf-native.nvim`:\n" .. err_fzf, vim.log.levels.ERROR, {})
-			end
 		end,
 	},
 	{
