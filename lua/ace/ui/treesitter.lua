cfda489393aa4e59fe9d320531a78be5219e2fcc
ace <vuong753333@gmail.com>
2024-08-28 11:25:28 +0700

    refactor: update treesitter setting

--- lua/ace/ui/treesitter.lua
+++ lua/ace/ui/treesitter.lua
@@ -7,6 +7,8 @@ return {
 		require("lazy.core.loader").add_to_rtp(plugin)
 		require("nvim-treesitter.query_predicates")
 	end,
+	cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+	opts_extend = { "ensure_installed" },
 	opts = {
 		ensure_installed = {
 			"c",
@@ -34,6 +36,9 @@ return {
 		ignore_install = {},
 	},
 	config = function(_, opts)
+		if type(opts.ensure_installed) == "table" then
+			opts.ensure_installed = Ace.dedup(opts.ensure_installed)
+		end
 		require("nvim-treesitter.configs").setup(opts)
 	end,
 }
