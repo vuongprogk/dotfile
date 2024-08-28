1e06029abd8de7653d07c2d998be0f7804e1de63
ace <vuong753333@gmail.com>
2024-08-28 11:23:14 +0700

    feat: comback luasnip

---
+++ lua/ace/plugins/luasnip.lua
@@ -0,0 +1,57 @@
+return {
+	{
+		"L3MON4D3/LuaSnip",
+		lazy = true,
+		build = (not Ace.is_win())
+				and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build'; make install_jsregexp"
+			or nil,
+		dependencies = {
+			{
+				"rafamadriz/friendly-snippets",
+				config = function()
+					require("luasnip.loaders.from_vscode").lazy_load()
+				end,
+			},
+			{
+				"nvim-cmp",
+				dependencies = {
+					"saadparwaiz1/cmp_luasnip",
+				},
+				opts = function(_, opts)
+					local luasnip = require("luasnip")
+					local cmp = require("cmp")
+					opts.snippet = {
+						expand = function(args)
+							luasnip.lsp_expand(args.body)
+						end,
+					}
+					table.insert(opts.sources, { name = "luasnip", max_item_count = 5 })
+					opts.mapping = vim.tbl_extend("force", opts.mapping, {
+						["<Tab>"] = cmp.mapping(function(fallback) -- super tab
+							if cmp.visible() then
+								cmp.select_next_item()
+							elseif luasnip.expand_or_jumpable() then
+								luasnip.expand_or_jump()
+							else
+								fallback()
+							end
+						end, { "i", "s" }),
+						["<S-Tab>"] = cmp.mapping(function(fallback)
+							if cmp.visible() then
+								cmp.select_prev_item()
+							elseif luasnip.jumpable(-1) then
+								luasnip.jump(-1)
+							else
+								fallback()
+							end
+						end, { "i", "s" }),
+					})
+				end,
+			},
+		},
+		opts = {
+			history = true,
+			delete_check_events = "TextChanged",
+		},
+	},
+}
