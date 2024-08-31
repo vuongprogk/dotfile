dde80028413cd1ec90414e28cb87480ac16eff99
ace <vuong753333@gmail.com>
2024-08-31 18:31:55 +0700

    feat: update gitsign config

--- lua/ace/editor/gitsigns.lua
+++ lua/ace/editor/gitsigns.lua
@@ -10,5 +10,21 @@ return {
 			remap = true,
 		},
 	},
-	opts = {},
+	opts = {
+		signs = {
+			add = { text = "▎" },
+			change = { text = "▎" },
+			delete = { text = "" },
+			topdelete = { text = "" },
+			changedelete = { text = "▎" },
+			untracked = { text = "▎" },
+		},
+		signs_staged = {
+			add = { text = "▎" },
+			change = { text = "▎" },
+			delete = { text = "" },
+			topdelete = { text = "" },
+			changedelete = { text = "▎" },
+		},
+	},
 }
