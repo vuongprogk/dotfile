c6539e11697f845810b20b51e7ad91f18c3836e4
vuongprogk <vuong753333@gmail.com>
2024-09-02 20:56:20 +0700

    feat: remove tailwind-sorter deprecated

--- lua/ace/lang/tailwind/tailwindcss.lua
+++ lua/ace/lang/tailwind/tailwindcss.lua
@@ -56,11 +56,4 @@ return {
 			end
 		end,
 	},
-	{
-		"laytan/tailwind-sorter.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-lua/plenary.nvim" },
-		build = "cd formatter && npm ci && npm run build",
-		config = true,
-	},
 }
