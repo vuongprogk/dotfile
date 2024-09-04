ea4d06d792ace48e2a0ff3e2855236946acf7160
vuongprogk <vuong753333@gmail.com>
2024-09-04 12:13:57 +0700

    feat: remove rainbow indent blank

--- lua/ace/ui/indent-blank.lua
+++ lua/ace/ui/indent-blank.lua
@@ -1,80 +1,29 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
 	event = { "BufReadPost", "BufWritePost", "BufNewFile" },
-	dependencies = {
-		"HiPhish/rainbow-delimiters.nvim",
-	},
-	main = "ibl",
 	opts = function()
-		local highlight = {
-			"RainbowRed",
-			"RainbowYellow",
-			"RainbowBlue",
-			"RainbowOrange",
-			"RainbowGreen",
-			"RainbowViolet",
-			"RainbowCyan",
-		}
-
-		local hooks = require("ibl.hooks")
-		hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-			vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-			vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-			vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-			vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-			vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-			vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-			vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
-		end)
-
-		-- TODO add rainbow_delimiters
-		local rainbow_delimiters = require("rainbow-delimiters")
-
-		vim.g.rainbow_delimiters = {
-			strategy = {
-				[""] = rainbow_delimiters.strategy["global"],
-				vim = rainbow_delimiters.strategy["local"],
-			},
-			query = {
-				[""] = "rainbow-delimiters",
-				lua = "rainbow-blocks",
-			},
-			priority = {
-				[""] = 110,
-				lua = 210,
-			},
-
-			highlight = highlight,
-		}
 		return {
 			indent = {
-				highlight = highlight,
 				char = "│",
 				tab_char = "│",
-				smart_indent_cap = true,
 			},
-			whitespace = {
-				highlight = highlight,
-				remove_blankline_trail = false,
-			},
-			scope = {
-				enabled = false,
-			},
+			scope = { show_start = false, show_end = false, enabled = false },
 			exclude = {
 				filetypes = {
 					"help",
-					"NvimTree",
 					"alpha",
 					"dashboard",
+					"neo-tree",
 					"Trouble",
 					"trouble",
 					"lazy",
 					"mason",
 					"notify",
 					"toggleterm",
-					"checkhealth",
+					"lazyterm",
 				},
 			},
 		}
 	end,
+	main = "ibl",
 }
