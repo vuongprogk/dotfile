f5d37ca1ad5ce01983dca7b83a87db0f5d87a61e
vuongprogk <vuong753333@gmail.com>
2024-09-04 12:39:32 +0700

    feat: update dashboard

--- lua/ace/ui/dashboard.lua
+++ lua/ace/ui/dashboard.lua
@@ -3,15 +3,14 @@ return {
 	lazy = false, -- As https://github.com/nvimdev/dashboard-nvim/pull/450, dashboard-nvim shouldn't be lazy-loaded to properly handle stdin.
 	-- enabled = false,
 	opts = function()
-		local logo = [[ 
-         █████╗  ██████╗███████╗
-			██╔══██╗██╔════╝██╔════╝
-			███████║██║     █████╗  
-			██╔══██║██║     ██╔══╝  
-			██║  ██║╚██████╗███████╗
-			╚═╝  ╚═╝ ╚═════╝╚══════╝
-    ]]
-
+		local logo = [[
+     █████╗  ██████╗███████╗
+    ██╔══██╗██╔════╝██╔════╝
+    ███████║██║     █████╗  
+    ██╔══██║██║     ██╔══╝  
+    ██║  ██║╚██████╗███████╗
+    ╚═╝  ╚═╝ ╚═════╝╚══════╝
+  ]]
 		logo = string.rep("\n", 8) .. logo .. "\n\n"
 
 		local opts = {
