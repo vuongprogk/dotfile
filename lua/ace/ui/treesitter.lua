c313155d483c42d0a6853243fe9b88884724fbc5
vuongprogk <vuong753333@gmail.com>
2024-09-04 12:17:06 +0700

    feat: treesitter update config

--- lua/ace/ui/treesitter.lua
+++ lua/ace/ui/treesitter.lua
@@ -1,44 +1,126 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	event = { "BufReadPost", "BufWritePost", "BufNewFile" },
-	build = ":TSUpdate",
-	lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
-	init = function(plugin)
-		require("lazy.core.loader").add_to_rtp(plugin)
-		require("nvim-treesitter.query_predicates")
-	end,
-	cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-	opts_extend = { "ensure_installed" },
-	opts = {
-		ensure_installed = {
-			"c",
-			"vim",
-			"cpp",
-			"python",
-			"css",
-			"html",
-			"lua",
-			"markdown_inline",
-			"latex",
-			"regex",
-			"markdown",
-			"json",
-			"bash",
-			"javascript",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+		build = ":TSUpdate",
+		lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
+		init = function(plugin)
+			require("lazy.core.loader").add_to_rtp(plugin)
+			require("nvim-treesitter.query_predicates")
+		end,
+		cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+		keys = {
+			{ "<c-space>", desc = "Increment Selection" },
+			{ "<bs>", desc = "Decrement Selection", mode = "x" },
 		},
-		sync_install = true,
-		auto_install = true,
-		highlight = {
-			enable = true,
-			-- disable = { "c_sharp" },
+		opts_extend = { "ensure_installed" },
+		opts = {
+			ensure_installed = {
+				"cpp",
+				"python",
+				"css",
+				"html",
+				"lua",
+				"latex",
+				"bash",
+				"c",
+				"diff",
+				"javascript",
+				"jsdoc",
+				"json",
+				"jsonc",
+				"luadoc",
+				"luap",
+				"markdown",
+				"markdown_inline",
+				"printf",
+				"query",
+				"regex",
+				"toml",
+				"tsx",
+				"typescript",
+				"vim",
+				"vimdoc",
+				"xml",
+				"yaml",
+			},
+			sync_install = true,
+			auto_install = true,
+			highlight = {
+				enable = true,
+				-- disable = { "c_sharp" },
+			},
+			indent = { enable = true },
+			ignore_install = {},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
+			},
+			textobjects = {
+				move = {
+					enable = true,
+					goto_next_start = {
+						["]f"] = "@function.outer",
+						["]c"] = "@class.outer",
+						["]a"] = "@parameter.inner",
+					},
+					goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+					goto_previous_start = {
+						["[f"] = "@function.outer",
+						["[c"] = "@class.outer",
+						["[a"] = "@parameter.inner",
+					},
+					goto_previous_end = {
+						["[F"] = "@function.outer",
+						["[C"] = "@class.outer",
+						["[A"] = "@parameter.inner",
+					},
+				},
+			},
 		},
-		indent = { enable = true },
-		ignore_install = {},
+		config = function(_, opts)
+			if type(opts.ensure_installed) == "table" then
+				opts.ensure_installed = Ace.dedup(opts.ensure_installed)
+			end
+			require("nvim-treesitter.configs").setup(opts)
+		end,
 	},
-	config = function(_, opts)
-		if type(opts.ensure_installed) == "table" then
-			opts.ensure_installed = Ace.dedup(opts.ensure_installed)
-		end
-		require("nvim-treesitter.configs").setup(opts)
-	end,
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		event = "VeryLazy",
+		enabled = true,
+		config = function()
+			-- If treesitter is already loaded, we need to run config again for textobjects
+			if Ace.is_loaded("nvim-treesitter") then
+				local opts = Ace.opts("nvim-treesitter")
+				require("nvim-treesitter.configs").setup({ textobjects = opts.textobjects })
+			end
+
+			-- When in diff mode, we want to use the default
+			-- vim text objects c & C instead of the treesitter ones.
+			local move = require("nvim-treesitter.textobjects.move") ---@type table<string,fun(...)>
+			local configs = require("nvim-treesitter.configs")
+			for name, fn in pairs(move) do
+				if name:find("goto") == 1 then
+					move[name] = function(q, ...)
+						if vim.wo.diff then
+							local config = configs.get_module("textobjects.move")[name] ---@type table<string,string>
+							for key, query in pairs(config or {}) do
+								if q == query and key:find("[%]%[][cC]") then
+									vim.cmd("normal! " .. key)
+									return
+								end
+							end
+						end
+						return fn(q, ...)
+					end
+				end
+			end
+		end,
+	},
 }
