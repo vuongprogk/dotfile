92b0b13d74eb27271c783234a5d5708ed795d831
vuongprogk <vuong753333@gmail.com>
2024-09-07 02:19:20 +0700

    feat: update neotree

--- lua/ace/editor/neo-tree.lua
+++ lua/ace/editor/neo-tree.lua
@@ -118,13 +118,5 @@ return {
 			},
 		})
 		require("neo-tree").setup(opts)
-		vim.api.nvim_create_autocmd("TermClose", {
-			pattern = "*lazygit",
-			callback = function()
-				if package.loaded["neo-tree.sources.git_status"] then
-					require("neo-tree.sources.git_status").refresh()
-				end
-			end,
-		})
 	end,
 }
