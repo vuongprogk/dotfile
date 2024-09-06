e19ff47ff76456483f3bc82ebfdaf2cf5fbb62f5
vuongprogk <vuong753333@gmail.com>
2024-09-07 02:23:18 +0700

    feat: update cmp formating

--- lua/ace/coding/nvim-cmp.lua
+++ lua/ace/coding/nvim-cmp.lua
@@ -1,11 +1,11 @@
 return {
 	"hrsh7th/nvim-cmp",
-	version = false, -- last release is way too oldevent = "InsertEnter",
+	version = false, -- last release is way too old
+	event = "InsertEnter",
 	dependencies = {
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
-		"onsails/lspkind.nvim",
 	},
 	opts = function()
 		vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
@@ -37,27 +37,25 @@ return {
 			},
 			sorting = defaults.sorting,
 			formatting = {
-				format = require("lspkind").cmp_format({
-					mode = "symbol_text",
-					maxwidth = 50,
-					show_labelDetails = false,
-					-- TODO: Refactor this
-					menu = {
-						nvim_lsp = "[LSP]",
-						path = "[Path]",
-						nvim_lua = "[Lua]",
-						buffer = "[Buffer]",
-						emoji = "[Emoji]",
-						luasnip = "[LuaSnip]",
-						copilot = "[Copilot]",
-					},
-					before = function(entry, vim_item)
-						if Ace.is_loaded("tailwindcss-colorizer-cmp.nvim") then
-							vim_item = require("tailwindcss-colorizer-cmp").formatter(entry, vim_item)
+				format = function(entry, item)
+					local icons = Ace.config.icons.kinds
+					if icons[item.kind] then
+						item.kind = icons[item.kind] .. item.kind
+					end
+
+					local widths = {
+						abbr = vim.g.cmp_widths and vim.g.cmp_widths.abbr or 40,
+						menu = vim.g.cmp_widths and vim.g.cmp_widths.menu or 30,
+					}
+
+					for key, width in pairs(widths) do
+						if item[key] and vim.fn.strdisplaywidth(item[key]) > width then
+							item[key] = vim.fn.strcharpart(item[key], 0, width - 1) .. "…"
 						end
-						return vim_item
-					end,
-				}),
+					end
+
+					return item
+				end,
 			},
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
