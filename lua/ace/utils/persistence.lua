b8c4835c03f207989cbb37ec09a470601c3ccd0e
vuongprogk <vuong753333@gmail.com>
2024-09-09 01:38:33 +0700

    refactor: change keymap

--- lua/ace/utils/persistence.lua
+++ lua/ace/utils/persistence.lua
@@ -2,46 +2,11 @@ return {
 	"folke/persistence.nvim",
 	event = "BufReadPre",
 	opts = {},
-	keys = {
-		{
-			"<leader>wr",
-			function()
-				require("persistence").load()
-			end,
-			desc = "Restore Session",
-			remap = true,
-		},
-		{
-			"<leader>wl",
-			function()
-				require("persistence").load({ last = true })
-			end,
-			desc = "Restore Session",
-			remap = true,
-		},
-		{
-			"<leader>ls",
-			function()
-				require("persistence").select()
-			end,
-			desc = "Select Session",
-			remap = true,
-		},
-		{
-			"<leader>wd",
-			function()
-				require("persistence").stop()
-			end,
-			desc = "Stop saving session",
-			remap = true,
-		},
-		{
-			"<leader>ws",
-			function()
-				require("persistence").save()
-			end,
-			desc = "Saving session",
-			remap = true,
-		},
-	},
+-- stylua: ignore
+  keys = {
+    { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
+    { "<leader>qS", function() require("persistence").select() end,desc = "Select Session" },
+    { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+    { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
+  },
 }
