aeb2b12611e263397e8619b3ad74e08535f28733
vuongprogk <vuong753333@gmail.com>
2024-09-09 01:38:38 +0700

    feat: change keymap

--- lua/ace/editor/todo-comment.lua
+++ lua/ace/editor/todo-comment.lua
@@ -3,21 +3,14 @@ return {
 	cmd = { "TodoTelescope" },
 	event = { "BufReadPost", "BufWritePost", "BufNewFile" },
 	dependencies = { "nvim-lua/plenary.nvim" },
-	keys = {
-		{
-			"]t",
-			function()
-				require("todo-comments").jump_next()
-			end,
-			desc = "Next Todo Comment",
-		},
-		{
-			"[t",
-			function()
-				require("todo-comments").jump_prev()
-			end,
-			desc = "Previous Todo Comment",
-		},
-	},
+   -- stylua: ignore
+    keys = {
+      { "]t", function() require("todo-comments").jump_next() end, desc = "Next Todo Comment" },
+      { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous Todo Comment" },
+      { "<leader>xt", "<cmd>Trouble todo toggle<cr>", desc = "Todo (Trouble)" },
+      { "<leader>xT", "<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
+      { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+      { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+    },
 	opts = {},
 }
