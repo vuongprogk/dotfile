a94c589f5213512ed7cb11f934fea35654dff390
vuongprogk <vuong753333@gmail.com>
2024-09-10 02:01:57 +0700

    refactor: lang rust config

--- lua/ace/lang/rust/rust.lua
+++ lua/ace/lang/rust/rust.lua
@@ -21,4 +21,82 @@ return {
 			table.insert(opts.sources, { name = "crates" })
 		end,
 	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = { ensure_installed = { "rust", "ron" } },
+	},
+	{
+		"williamboman/mason.nvim",
+		optional = true,
+		opts = { ensure_installed = { "codelldb" } },
+	},
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^5", -- Recommended
+		opts = {
+			server = {
+				on_attach = function(_, bufnr)
+					vim.keymap.set("n", "<leader>cR", function()
+						vim.cmd.RustLsp("codeAction")
+					end, { desc = "Code Action", buffer = bufnr })
+					vim.keymap.set("n", "<leader>dr", function()
+						vim.cmd.RustLsp("debuggables")
+					end, { desc = "Rust Debuggables", buffer = bufnr })
+				end,
+				default_settings = {
+					-- rust-analyzer language server configuration
+					["rust-analyzer"] = {
+						cargo = {
+							allFeatures = true,
+							loadOutDirsFromCheck = true,
+							buildScripts = {
+								enable = true,
+							},
+						},
+						-- Add clippy lints for Rust.
+						checkOnSave = true,
+						procMacro = {
+							enable = true,
+							ignored = {
+								["async-trait"] = { "async_trait" },
+								["napi-derive"] = { "napi" },
+								["async-recursion"] = { "async_recursion" },
+							},
+						},
+					},
+				},
+			},
+		},
+		config = function(_, opts)
+			vim.g.rustaceanvim = vim.tbl_deep_extend("keep", vim.g.rustaceanvim or {}, opts or {})
+			if vim.fn.executable("rust-analyzer") == 0 then
+				Ace.error(
+					"**rust-analyzer** not found in PATH, please install it.\nhttps://rust-analyzer.github.io/",
+					{ title = "rustaceanvim" }
+				)
+			end
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				taplo = {
+					keys = {
+						{
+							"K",
+							function()
+								if vim.fn.expand("%:t") == "Cargo.toml" and require("crates").popup_available() then
+									require("crates").show_popup()
+								else
+									vim.lsp.buf.hover()
+								end
+							end,
+							desc = "Show Crate Documentation",
+						},
+					},
+				},
+			},
+		},
+	},
 }
