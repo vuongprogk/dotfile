5605c03c3f49e30cd84b4ccf663484c54689f278
vuongprogk <vuong753333@gmail.com>
2024-09-13 18:24:12 +0700

    refactor: using tailwind-tools may be better

--- lua/ace/lang/tailwindcss/tailwindcss.lua
+++ lua/ace/lang/tailwindcss/tailwindcss.lua
@@ -1,47 +1,11 @@
 return {
+	-- tailwind-tools.lua
 	{
-		"neovim/nvim-lspconfig",
-		opts = {
-			servers = {
-				tailwindcss = {
-					-- exclude a filetype from the default_config
-					filetypes_exclude = { "markdown" },
-					-- add additional filetypes to the default_config
-					filetypes_include = {},
-					-- to fully override the default_config, change the below
-					-- filetypes = {}
-				},
-			},
-			setup = {
-				tailwindcss = function(_, opts)
-					local tw = require("lspconfig.server_configurations.tailwindcss")
-					opts.filetypes = opts.filetypes or {}
-
-					-- Add default filetypes
-					vim.list_extend(opts.filetypes, tw.default_config.filetypes)
-
-					-- Remove excluded filetypes
-					--- @param ft string
-					opts.filetypes = vim.tbl_filter(function(ft)
-						return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
-					end, opts.filetypes)
-
-					-- Additional settings for Phoenix projects
-					opts.settings = {
-						tailwindCSS = {
-							includeLanguages = {
-								elixir = "html-eex",
-								eelixir = "html-eex",
-								heex = "html-eex",
-							},
-						},
-					}
-
-					-- Add additional filetypes
-					vim.list_extend(opts.filetypes, opts.filetypes_include or {})
-				end,
-			},
-		},
+		"luckasRanarison/tailwind-tools.nvim",
+		event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+		name = "tailwind-tools",
+		build = ":UpdateRemotePlugins",
+		opts = {}, -- your configuration
 	},
 	{
 		"hrsh7th/nvim-cmp",
