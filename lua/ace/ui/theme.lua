27c2c480b6098fceb5a8f16c6ce9a67cb8913e6e
vuongprogk <vuong753333@gmail.com>
2024-09-14 02:35:23 +0700

    refactor: update theme config setup

--- lua/ace/ui/theme.lua
+++ lua/ace/ui/theme.lua
@@ -3,7 +3,16 @@ return {
 	lazy = false,
 	priority = 1000,
 	opts = {
-		style = "night", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
+		style = "night",
+		styles = {
+			comments = { italic = true },
+			keywords = { italic = true, bold = true },
+			functions = { italic = true, bold = true },
+		},
+		on_colors = function(colors)
+			colors.hint = colors.orange
+			colors.error = "#ff0000"
+		end,
 	},
 	config = function(_, opts)
 		require("tokyonight").load(opts)
