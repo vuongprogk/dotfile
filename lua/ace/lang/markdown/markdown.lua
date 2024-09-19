531ba53367edfe591d960d8e12aa91f5ba3d3bd5
vuongprogk <vuong753333@gmail.com>
2024-09-20 01:43:24 +0700

    feat: add markdown plugin

--- lua/ace/lang/markdown/markdown.lua
+++ lua/ace/lang/markdown/markdown.lua
@@ -11,20 +11,73 @@ return {
 			},
 		},
 	},
+	-- {
+	-- 	"MeanderingProgrammer/render-markdown.nvim",
+	-- 	opts = {
+	-- 		file_types = { "markdown", "norg", "rmd", "org" },
+	-- 		checkbox = {
+	-- 			custom = {
+	-- 				followup = { raw = "[>]", rendered = " ", highlight = "RenderMarkdownWarn" },
+	-- 				cancel = { raw = "[~]", rendered = "󰰱 ", highlight = "RenderMarkdownError" },
+	-- 				important = { raw = "[!]", rendered = " ", highlight = "RenderMarkdownError" },
+	-- 			},
+	-- 		},
+	-- 	},
+	-- 	ft = { "markdown", "norg", "rmd", "org" },
+	-- },
+	-- Markdown preview
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = function()
+			require("lazy").load({ plugins = { "markdown-preview.nvim" } })
+			vim.fn["mkdp#util#install"]()
+		end,
+		keys = {
+			{
+				"<leader>cp",
+				ft = "markdown",
+				"<cmd>MarkdownPreviewToggle<cr>",
+				desc = "Markdown Preview",
+			},
+		},
+		config = function()
+			vim.cmd([[do FileType]])
+		end,
+	},
+
 	{
 		"MeanderingProgrammer/render-markdown.nvim",
-		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
 		opts = {
 			file_types = { "markdown", "norg", "rmd", "org" },
-			checkbox = {
-				custom = {
-					followup = { raw = "[>]", rendered = " ", highlight = "RenderMarkdownWarn" },
-					cancel = { raw = "[~]", rendered = "󰰱 ", highlight = "RenderMarkdownError" },
-					important = { raw = "[!]", rendered = " ", highlight = "RenderMarkdownError" },
-				},
+			code = {
+				sign = false,
+				width = "block",
+				right_pad = 1,
+			},
+			heading = {
+				sign = false,
+				icons = {},
 			},
 		},
 		ft = { "markdown", "norg", "rmd", "org" },
+		config = function(_, opts)
+			require("render-markdown").setup(opts)
+			Ace.toggle.map("<leader>um", {
+				name = "Render Markdown",
+				get = function()
+					return require("render-markdown.state").enabled
+				end,
+				set = function(enabled)
+					local m = require("render-markdown")
+					if enabled then
+						m.enable()
+					else
+						m.disable()
+					end
+				end,
+			})
+		end,
 	},
 	{
 		"stevearc/conform.nvim",
