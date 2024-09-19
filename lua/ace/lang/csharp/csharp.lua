1755af73d19e193aa895ab35329d41800b058dee
vuongprogk <vuong753333@gmail.com>
2024-09-20 01:43:40 +0700

    feat: add keymap for csharp config

--- lua/ace/lang/csharp/csharp.lua
+++ lua/ace/lang/csharp/csharp.lua
@@ -27,6 +27,17 @@ return {
 							return require("omnisharp_extended").handler(...)
 						end,
 					},
+					keys = {
+						{
+							"gd",
+							Ace.has("telescope.nvim") and function()
+								require("omnisharp_extended").telescope_lsp_definitions()
+							end or function()
+								require("omnisharp_extended").lsp_definitions()
+							end,
+							desc = "Goto Definition",
+						},
+					},
 					enable_roslyn_analyzers = true,
 					organize_imports_on_format = true,
 					enable_import_completion = true,
