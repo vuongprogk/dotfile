0f7d49314c93d4ab869038c4a10b072bd47260be
vuongprogk <vuong753333@gmail.com>
2024-09-20 01:45:17 +0700

    feat: add better select item with c-n and c-p

--- lua/ace/coding/nvim-cmp.lua
+++ lua/ace/coding/nvim-cmp.lua
@@ -20,6 +20,8 @@ return {
 					["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+					["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
 					["<C-s>"] = cmp.mapping.complete(), -- show completion suggestions because some terminal emulator not word
 					["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
 					["<C-e>"] = cmp.mapping.abort(), -- close completion window
