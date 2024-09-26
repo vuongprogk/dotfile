712bfd9d809b5c9c2867df4292209dbc6ba56ed7
vuongprogk <vuong753333@gmail.com>
2024-09-27 01:47:28 +0700

    refactor: update keymap config

--- lua/ace/utils/mason.lua
+++ lua/ace/utils/mason.lua
@@ -1,6 +1,8 @@
 return {
 	"williamboman/mason.nvim",
 	cmd = "Mason",
+	keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+	build = ":MasonUpdate",
 	opts_extend = { "ensure_installed" },
 	opts = {
 		ui = {
